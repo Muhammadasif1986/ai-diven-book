53b64a9218c22baa1231f79b1b176397e7eaf779
MUHAMMAD ASIF <162907407+Muhammadasif1986@users.noreply.github.com>
2025-12-12 20:57:33 +0500

    .

---
+++ tech-book/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("D:\\ai-diven-book\\tech-book\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("D:\\ai-diven-book\\tech-book\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\ai-diven-book\\tech-book\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\ai-diven-book\\tech-book\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\ai-diven-book\\tech-book\\src\\css\\custom.css"),
+];
